c0e5bebddad88baa7e59d2a4db355a9be74c8035
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-08-12 11:26:33 +0530

    Create 1 hello world.java

---
+++ basic/1 hello world.java
@@ -0,0 +1,7 @@
+public class Main // java always start with class and class name shout be same as file name
+{
+    public static void main(String[] args) // entry point in java ( same as main function in c)
+    {
+        System.out.println("Hello world!! ");
+    }
+}
