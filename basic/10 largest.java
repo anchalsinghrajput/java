466b44b2c8e3e8a1b9eb4d5d20346d1da87704ee
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-08-17 23:17:08 +0530

    Create 10 largest.java

---
+++ basic/10 largest.java
@@ -0,0 +1,18 @@
+//Take integer inputs till the user enters 0 and print the largest number from all
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("Enter elements : ");
+		Scanner sc = new Scanner (System.in);
+		int ele,large=-1,i,sum=0;
+	    do{
+	        ele=sc.nextInt(); 
+	        if(ele>large)
+	        {
+	            large=ele;    
+	        }
+	    }while(ele!=0);
+		System.out.print("Large : " +large);
+	}
+}
