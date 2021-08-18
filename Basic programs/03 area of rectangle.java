01de51e60a94c506c3327656220203c6f33429d0
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-08-18 11:25:39 +0530

    Create 03 area of rectangle.java

---
+++ Basic programs/03 area of rectangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("Enter the length and width of the rectangle : ");
+		Scanner sc = new Scanner(System.in);
+		float length = sc.nextFloat();
+		float width = sc.nextFloat();
+		float area = length * width;
+		System.out.print("Area of the rectangle : "+area +" sq.units");
+	}
+}
